902e1504253c60e5584b5b5ddaca4e45619f2e37
nguyenduthpt@gmail.com <nguyenduthpt@gmail.com@936c44b6-bfb6-d5d7-2852-32d05bcbebeb>
2013-05-23 16:15:39 +0000

    gmap_extract

---
+++ trunk/gmap_extract/gmap_extract.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        gmap_extract
+// @namespace   infos_extract
+// @description extract infor from g-maps
+// @include     https://maps.google.com/*
+// @include     http://maps.google.com/*
+// @version     1
+// ==/UserScript==
