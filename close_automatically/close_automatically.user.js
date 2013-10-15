d6db6dbcf53b007a34931f23a79c7d6c2a887ccb
nguyenduthpt <nguyenduthpt@gmail.com>
2013-10-15 08:26:47 +0000

    Cp nhat

--- close_automatically/close_automatically.user.js
+++ close_automatically/close_automatically.user.js
@@ -3,7 +3,10 @@
 // @namespace   auto_action
 // @include     http://www.cfdtradingnews.info/gemail.php*
 // @include     http://free.chamthi.net/gemail.php*
+// @include     http://localhost/gmap/gemail.php*
+// @include     http://localhost/gmap/getemail.php*
+// @include     http://localhost/gmap/gplus.php*
 // @version     1
 // ==/UserScript==
 
-setTimeout(close,7000);
+setTimeout(close,6000);
